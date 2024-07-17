31e7f011da9b04ef80a8dd1ef2c8e049f2e4fda9
dyadyasawa <dyadyasawa@gmail.com>
2024-07-17 16:31:10 +0300

    Работа над проектом.

--- users/urls.py
+++ users/urls.py
@@ -6,16 +6,8 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
-    # path("register/", UserCreateAPIView.as_view(), name="register"),
-    # path(
-    #     "login/",
-    #     TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
-    #     name="login",
-    # ),
-    # path(
-    #     "token/refresh/",
-    #     TokenRefreshView.as_view(permission_classes=(AllowAny,)),
-    #     name="token_refresh",
-    # ),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login",),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh",),
 ]
 

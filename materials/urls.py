2888f4cdf45f81a76566b6021bc5029704c6137d
dyadyasawa <dyadyasawa@gmail.com>
2024-07-20 01:48:10 +0300

    Работа над проектом.

--- materials/urls.py
+++ materials/urls.py
@@ -4,10 +4,13 @@ from django.urls import path
 # from rest_framework_simplejwt.views import
 
 from materials.apps import MaterialsConfig
-from materials.views import StartPageView
+from materials.views import StartPageView, CoursesListView, CourseDetailView
 
 app_name = MaterialsConfig.name
 
 urlpatterns = [
     path("", StartPageView.as_view(), name="start_page"),
+
+    path("courses_list/", CoursesListView.as_view(), name="courses_list"),
+    path("course_detail/<int:pk>/", CourseDetailView.as_view(), name="course_detail"),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from django.views.generic import (
     TemplateView,
@@ -10,6 +10,26 @@ from django.views.generic import (
     DeleteView,
 )
 
+from materials.models import Course, Lesson
+
 
 class StartPageView(TemplateView):
     template_name = "materials_app/start_page.html"
+
+
+class CoursesListView(ListView):
+    model = Course
+    template_name = "materials_app/courses_list.html"
+
+
+class CourseDetailView(ListView):
+    model = Course
+    template_name = "materials_app/course_detail.html"
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        c = Course.objects.get(pk=self.kwargs.get('pk'))
+
+        queryset = c.lesson_set.all()
+
+        return queryset

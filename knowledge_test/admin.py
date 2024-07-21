bb09ac4a7989a7a985268b0cbe9761b5f5ec428d
dyadyasawa <dyadyasawa@gmail.com>
2024-07-21 13:58:16 +0300

    Админка и фикстуры для модели тестирования. Миграции.

--- knowledge_test/admin.py
+++ knowledge_test/admin.py
@@ -1,3 +1,25 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from knowledge_test.models import CourseTest, Question, Answer
+
+
+@admin.register(CourseTest)
+class CourseTestAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text",)
+    list_filter = ("question_text",)
+    search_fields = ("question_text",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("answer_text",)
+    list_filter = ("answer_text",)
+    search_fields = ("answer_text",)
--- knowledge_test/models.py
+++ knowledge_test/models.py
@@ -1,3 +1,43 @@
+
 from django.db import models
 
-# Create your models here.
+from materials.models import Course
+
+# NULLABLE = {"null": True, "blank": True}
+
+
+class CourseTest(models.Model):
+    name = models.CharField(max_length=250, verbose_name="Название теста")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+
+    class Meta:
+        verbose_name = "Тест"
+        verbose_name_plural = "Тесты"
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    course_test = models.ForeignKey(CourseTest, on_delete=models.CASCADE, verbose_name="Тест")
+    question_text = models.CharField(max_length=300, verbose_name="Текст вопроса")
+
+    class Meta:
+        verbose_name = "вопрос"
+        verbose_name_plural = "вопросы"
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Вопрос")
+    answer_text = models.CharField(max_length=300, verbose_name="Текст ответа")
+    is_correct = models.BooleanField(default=False, verbose_name="Правильность ответа")
+
+    class Meta:
+        verbose_name = "ответ"
+        verbose_name_plural = "ответы"
+
+    def __str__(self):
+        return f"{self.answer_text}({self.is_correct})"
